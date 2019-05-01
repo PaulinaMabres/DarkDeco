7e0b58b9b4065f422e61d13fcdd4bed7e554a4cb
agostinacarla <agostinademarziani@yahoo.com.ar>
2019-04-30 22:59:59 -0300

    Login
    Se actualizo archivos de login y registro con nuevo formato

--- login.php
+++ login.php
@@ -1,61 +1,87 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html>
 <head>
-  <meta charset="utf-8">
-  <?php
-  include('partials/headmaster.php');
-  ?>
-  <link rel="stylesheet" href="css/master.css">
-  <link rel="stylesheet" href="css/home.css">
-  <title>Dark Déco</title>
+	<meta charset="utf-8">
+	<title>Iniciar Sesión</title>
+   <!--Made with love by Mutiullah Samim -->
+
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+
 </head>
 <body>
-  <?php
-  include('partials/header.php');
-  ?>
-  <div class="B col-md-4">
 
-  </div>
-  <section class="col-md-4 col-xs-6 formulario login">
-    <h1>Iniciar Sesión</h1>
-    <form class="" action="./home.php" method="post">
-      <div class="form-group">
-        <input class="form-control" type="text" name="e-mail" value="" placeholder="e-mail">
-      </div>
-      <div class="form-group">
-        <input class="form-control" type="text" name="contraseña" value="" placeholder="contraseña">
-      </div>
-      <button type="button" class="btn btn-default">Ingresar</button>
-      <div class="form-group">
-        <span><input type="checkbox" name=""> Recuérdame</span>
-      </div>
-      <button type="button" class="btn btn-default">Registrate</button>
-      <div class="form-group">
-        <a href="#">¿Has olvidado tu cuenta?</a>
-        <p>¿Eres nuevo en Dark Déco? <a href="#">Regístrate</a></p>
-      </div>
+<div class="container">
 
-      <div class="social-iconos">
-        <a class="btn btn-block btn-social btn-twitter">
-            <span class="fab fa-twitter"></span> Ingresá con Twitter
-        </a>
-        <a class="btn btn-block btn-social btn-facebook">
-            <span class="fab fa-facebook"></span> Ingresá con Facebook
-        </a>
-        <a class="btn btn-block btn-social btn-google">
-            <span class="fab fa-google"></span> Ingresá con Google
-        </a>
-      </div>
+	<?php
+		include('partials/header.php');
+	?>
 
-    </form>
-  </section>
-  <div class="A col-md-4 col-xs-6">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h4>Iniciar Sesión</h4>
+				 <div class="imagen" >
+				 	<img src="img/logoNuevo.png" alt="logo" width="100px" height="100px">
+				 </div>
 
-  </div>
+			</div>
+			<div class="card-body">
+				<form action="#" method="post">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="usuario">
 
-  <?php
-  // FOOTER
-  // include('partials/footer.php');
-  ?>
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="contraseña">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox">Recuérdame
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Ingresar" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					No tenés cuenta?<a href="register.php">Registrate</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Olvidaste tu contraseña?</a>
+				</div>
+
+				<div class="d-flex justify-content-start social_icon">
+						<span><i class="fab fa-facebook-square"></i></span>
+						<span><i class="fab fa-google-plus-square"></i></span>
+						<span><i class="fab fa-twitter-square"></i></span>
+					</div>
+	      </div>
+
+			</div>
+		</div>
+	</div>
+
+
+</div>
+
+<?php
+	// FOOTER
+	include('partials/footer.php');
+?>
+
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Registro</title>
+   <!--Made with love by Mutiullah Samim -->
+
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+</head>
+<body>
+
+<div class="container">
+	<?php
+		include('partials/header.php');
+	?>
+
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h4>Registro</h4>
+			</div>
+			<div class="imagen" >
+			 <img src="img/logoNuevo.png" alt="logo" width="100px" height="100px">
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="form-group">
+		        <div class="input-group mb-3">
+		          <input type="text" class="form-control" placeholder="tu nombre" aria-label="tu nombre" aria-describedby="basic-addon1">
+		        </div>
+		      </div>
+		      <div class="form-group">
+		          <input type="text" class="form-control" placeholder="tu apellido" aria-label="tu apellido" aria-describedby="basic-addon1">
+		      </div>
+		      <div class="form-group">
+		          <input type="email" class="form-control" placeholder="tu email" aria-label="tu email" aria-describedby="basic-addon1">
+		      </div>
+		      <div class="form-group">
+		          <input type="password" class="form-control" placeholder="contraseña" aria-label="contraseña" aria-describedby="basic-addon1">
+		      </div>
+		      <div class="form-group">
+		          <input type="password" class="form-control" placeholder="confirmación de contraseña" aria-label="confirmación de contraseña" aria-describedby="basic-addon1">
+		      </div>
+					<div class="form-group">
+						<input type="submit" value="Confirmar" class="btn float-right login_btn">
+					</div>
+					<div class="form-group">
+						<a href="login.php"><input type="button" value="Volver" class="btn float-right login_btn"></a>
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+	// FOOTER
+	include('partials/footer.php');
+?>
+</body>
+</html>

069155ebe56a94b22214574550923ee9ec6f5e04
mlucasflores <ifloreslucas@gmail.com>
2019-04-30 21:30:29 -0300

    agrego header/css header y linkeo headmaster con header

--- partials/headmaster.php
+++ partials/headmaster.php
@@ -5,9 +5,11 @@
 <!-- Responsive!! -->
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Footer -->
+<!-- Footer / Header [Required links]-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+<link rel="stylesheet" href="css/header.css">
 <link rel="stylesheet" href="css/footer.css">
 <link rel="stylesheet" href="css/bootstrap-social.css">
 
